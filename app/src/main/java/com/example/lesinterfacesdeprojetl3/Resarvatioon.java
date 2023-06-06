fe961ef3be98b17198383ab4fceac3743598f1fa
hp <halousarra@gmail.com>
2023-06-06 10:30:52 +0100

    init5

--- app/src/main/java/com/example/lesinterfacesdeprojetl3/Resarvatioon.java
+++ app/src/main/java/com/example/lesinterfacesdeprojetl3/Resarvatioon.java
@@ -2,35 +2,111 @@ package com.example.lesinterfacesdeprojetl3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Resarvatioon extends AppCompatActivity {
+    DatePickerDialog.OnDateSetListener setListener;
     private EditText nomEditText,dateEdittext,timeEdittext,matriculEditText;
+    private int mHour,mMinute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resarvatioon);
         String nameprking = getIntent().getStringExtra("Name");
         Button rsrv = findViewById(R.id.btn_submit);
+
+
+
+
+
+
+
+
+        nomEditText = findViewById(R.id.et_name);
+        dateEdittext = findViewById(R.id.et_date);
+        timeEdittext = findViewById(R.id.et_time);
+        matriculEditText = findViewById(R.id.vcl);
+
+
+
+        String nom = nomEditText.getText().toString();
+        String matricule = matriculEditText.getText().toString();
+
+        String date = dateEdittext.getText().toString();
+        String time = timeEdittext.getText().toString();
+
+
+        Calendar calendar =Calendar.getInstance();
+        final int year =calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day =calendar.get(Calendar.DAY_OF_MONTH);
+
+
+     dateEdittext.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             DatePickerDialog datePickerDialog = new DatePickerDialog(Resarvatioon.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                     setListener, year, month, day);
+             datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+             datePickerDialog.show();
+         }
+     });
+
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String datee = day + "/" + month + "/" + year;
+                dateEdittext.setText(datee);
+            }
+        };
+
+
+        timeEdittext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar=Calendar.getInstance();
+                mHour=calendar.get(Calendar.HOUR);
+                mMinute=calendar.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog=new TimePickerDialog(Resarvatioon.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int h, int m) {
+                        timeEdittext.setText(h+":"+m);
+                    }
+                },mHour,mMinute,true);
+                timePickerDialog.show();
+
+            }
+        });
+
+
         rsrv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                nomEditText = findViewById(R.id.et_name);
-                dateEdittext = findViewById(R.id.et_date);
-                timeEdittext = findViewById(R.id.et_time);
-                matriculEditText = findViewById(R.id.vcl);
+
+
+
+
 
 
 
@@ -39,6 +115,27 @@ public class Resarvatioon extends AppCompatActivity {
 
                 String date = dateEdittext.getText().toString();
                 String time = timeEdittext.getText().toString();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
